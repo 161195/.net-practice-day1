be16032dcb4563a7a35c41ea7b11e8715d648eee
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-01 17:27:44 +0530

    letstry

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace trial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program world!");
+            Console.ReadLine();
+        }
+    }
+}
