c21b7eea1f93785ab5bcf2a95d477b27df54b43d
161195 <90571854+161195@users.noreply.github.com>
2021-10-01 23:18:30 +0530

    Update Program.cs
    made changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace trial1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("program world!");
+            Console.WriteLine("welcome to program world!");
             Console.ReadLine();
         }
     }
